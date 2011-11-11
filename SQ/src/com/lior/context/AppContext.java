25662aa2a864b1ad932b60990ecb4dcd7fbf5e3f
liorv <lior_v@yahoo.com>
2011-11-10 21:25:13 -0500

    asd

--- SQ/src/com/lior/context/AppContext.java
+++ SQ/src/com/lior/context/AppContext.java
@@ -2,12 +2,6 @@ package com.lior.context;
 
 import org.springframework.context.ApplicationContext;
 
-/**
- * This class provides application-wide access to the Spring ApplicationContext.
- * The ApplicationContext is injected by the class "ApplicationContextProvider".
- * 
- * @author Siegfried Bolz
- */
 public class AppContext
 {
 
